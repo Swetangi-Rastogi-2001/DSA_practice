8532297d9c9f02d66365511c934dc465494519d7
Swetangi Rastogi <87586921+Swetangi-Rastogi-2001@users.noreply.github.com>
2022-07-17 17:46:59 +0530

    Create longest_palindromic_subsequence.cpp

---
+++ dp/longest_palindromic_subsequence.cpp
@@ -0,0 +1,36 @@
+
+/*https://leetcode.com/problems/longest-palindromic-subsequence/*/
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string str1) {
+        
+        string str2= str1;
+        reverse(str2.begin(), str2.end());
+        
+        int n=str1.length();
+        int m=str2.length();
+        
+        int maxi=0;
+        
+        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+        for(int index1=0;index1<=n;index1++){
+            for(int index2=0;index2<=m;index2++){
+                if(index1==0||index2==0){
+                    dp[index1][index2]= 0;
+                }
+                else{
+                    if(str1[index1-1]==str2[index2-1]){
+                        dp[index1][index2]= 1+ dp[index1-1][index2-1];
+                        maxi= max(maxi, dp[index1][index2]);
+                    }
+                    else{
+                        dp[index1][index2]= max(dp[index1-1][index2], dp[index1][index2-1]);
+                    }
+                }
+            }
+        }
+        
+        return maxi;
+    }
+};
